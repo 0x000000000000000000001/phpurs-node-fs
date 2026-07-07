0105dba601167c7063d3c242f3356e1b56ea99da
0x1 <kevin.francart3@gmail.com>
2026-07-07 19:59:48 +0200

    Fix callback invocation by removing redundant thunk evaluation ()

--- src/Node/FS/Async.php
+++ src/Node/FS/Async.php
@@ -4,9 +4,9 @@ $readFileImpl = function($file, $opts, $cb) {
     $res = @file_get_contents($file);
     if ($res === false) {
         $err = new \Exception("Failed to read file: $file");
-        $cb($err, null)();
+        $cb($err, null);
     } else {
-        $cb(null, $res)();
+        $cb(null, $res);
     }
 };
 
@@ -14,9 +14,9 @@ $writeFileImpl = function($file, $buff, $opts, $cb) {
     $res = @file_put_contents($file, $buff);
     if ($res === false) {
         $err = new \Exception("Failed to write file: $file");
-        $cb($err, null)();
+        $cb($err, null);
     } else {
-        $cb(null, null)();
+        $cb(null, null);
     }
 };
 
@@ -24,9 +24,9 @@ $mkdirImpl = function($file, $opts, $cb) {
     $res = @mkdir($file, 0777, true);
     if (!$res && !is_dir($file)) {
         $err = new \Exception("Failed to create directory: $file");
-        $cb($err, null)();
+        $cb($err, null);
     } else {
-        $cb(null, null)();
+        $cb(null, null);
     }
 };
 
@@ -34,12 +34,12 @@ $readdirImpl = function($file, $cb) {
     $res = @scandir($file);
     if ($res === false) {
         $err = new \Exception("Failed to read directory: $file");
-        $cb($err, null)();
+        $cb($err, null);
     } else {
         $filtered = array_values(array_filter($res, function($item) {
             return $item !== '.' && $item !== '..';
         }));
-        $cb(null, $filtered)();
+        $cb(null, $filtered);
     }
 };
 
@@ -47,9 +47,9 @@ $renameImpl = function($old, $new, $cb) {
     $res = @rename($old, $new);
     if ($res === false) {
         $err = new \Exception("Failed to rename $old to $new");
-        $cb($err, null)();
+        $cb($err, null);
     } else {
-        $cb(null, null)();
+        $cb(null, null);
     }
 };
 
@@ -57,9 +57,9 @@ $unlinkImpl = function($file, $cb) {
     $res = @unlink($file);
     if ($res === false && file_exists($file)) {
         $err = new \Exception("Failed to unlink: $file");
-        $cb($err, null)();
+        $cb($err, null);
     } else {
-        $cb(null, null)();
+        $cb(null, null);
     }
 };
 
