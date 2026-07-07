497c27cfaabbb9e147cc9ac1aa137d62d05c9253
0x1 <kevin.francart3@gmail.com>
2026-07-07 19:13:37 +0200

    Add PHP FFI

---
+++ src/Node/FS/Async.php
@@ -0,0 +1,72 @@
+<?php
+
+$readFileImpl = function($file, $opts, $cb) {
+    $res = @file_get_contents($file);
+    if ($res === false) {
+        $err = new \Exception("Failed to read file: $file");
+        $cb($err, null)();
+    } else {
+        $cb(null, $res)();
+    }
+};
+
+$writeFileImpl = function($file, $buff, $opts, $cb) {
+    $res = @file_put_contents($file, $buff);
+    if ($res === false) {
+        $err = new \Exception("Failed to write file: $file");
+        $cb($err, null)();
+    } else {
+        $cb(null, null)();
+    }
+};
+
+$mkdirImpl = function($file, $opts, $cb) {
+    $res = @mkdir($file, 0777, true);
+    if (!$res && !is_dir($file)) {
+        $err = new \Exception("Failed to create directory: $file");
+        $cb($err, null)();
+    } else {
+        $cb(null, null)();
+    }
+};
+
+$readdirImpl = function($file, $cb) {
+    $res = @scandir($file);
+    if ($res === false) {
+        $err = new \Exception("Failed to read directory: $file");
+        $cb($err, null)();
+    } else {
+        $filtered = array_values(array_filter($res, function($item) {
+            return $item !== '.' && $item !== '..';
+        }));
+        $cb(null, $filtered)();
+    }
+};
+
+$renameImpl = function($old, $new, $cb) {
+    $res = @rename($old, $new);
+    if ($res === false) {
+        $err = new \Exception("Failed to rename $old to $new");
+        $cb($err, null)();
+    } else {
+        $cb(null, null)();
+    }
+};
+
+$unlinkImpl = function($file, $cb) {
+    $res = @unlink($file);
+    if ($res === false && file_exists($file)) {
+        $err = new \Exception("Failed to unlink: $file");
+        $cb($err, null)();
+    } else {
+        $cb(null, null)();
+    }
+};
+
+$exports['readFileImpl'] = $readFileImpl;
+$exports['writeFileImpl'] = $writeFileImpl;
+$exports['mkdirImpl'] = $mkdirImpl;
+$exports['readdirImpl'] = $readdirImpl;
+$exports['renameImpl'] = $renameImpl;
+$exports['unlinkImpl'] = $unlinkImpl;
+return $exports;
---
+++ src/Node/FS/Sync.php
@@ -0,0 +1,55 @@
+<?php
+
+$readFileImpl = function($file, $opts) {
+    $res = @file_get_contents($file);
+    if ($res === false) {
+        throw new \Exception("Failed to read file: $file");
+    }
+    return $res;
+};
+
+$writeFileImpl = function($file, $buff, $opts) {
+    $res = @file_put_contents($file, $buff);
+    if ($res === false) {
+        throw new \Exception("Failed to write file: $file");
+    }
+};
+
+$mkdirImpl = function($file, $opts) {
+    $res = @mkdir($file, 0777, true);
+    if (!$res && !is_dir($file)) {
+        throw new \Exception("Failed to create directory: $file");
+    }
+};
+
+$readdirImpl = function($file) {
+    $res = @scandir($file);
+    if ($res === false) {
+        throw new \Exception("Failed to read directory: $file");
+    }
+    return array_values(array_filter($res, function($item) {
+        return $item !== '.' && $item !== '..';
+    }));
+};
+
+$renameImpl = function($old, $new) {
+    $res = @rename($old, $new);
+    if ($res === false) {
+        throw new \Exception("Failed to rename $old to $new");
+    }
+};
+
+$unlinkImpl = function($file) {
+    $res = @unlink($file);
+    if ($res === false && file_exists($file)) {
+        throw new \Exception("Failed to unlink: $file");
+    }
+};
+
+$exports['readFileImpl'] = $readFileImpl;
+$exports['writeFileImpl'] = $writeFileImpl;
+$exports['mkdirImpl'] = $mkdirImpl;
+$exports['readdirImpl'] = $readdirImpl;
+$exports['renameImpl'] = $renameImpl;
+$exports['unlinkImpl'] = $unlinkImpl;
+return $exports;
